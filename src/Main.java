a33b57ca56c84b02de7de416cd57b54e24109669
KonstantinDobr <snastink@yandex.ru>
2023-10-12 20:20:56 +0300

    the best

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Люблю самостоятельные работы <3");
     }
 }
